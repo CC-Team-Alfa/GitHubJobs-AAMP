51c5e2e176fe46af86a80fd6544f023ff7fc38f6
MikolajLangner <mikilan123@gmail.com>
2019-12-02 12:18:00 +0100

    Minor changes with returning element

--- generatedDetailsCharacterView.js
+++ generatedDetailsCharacterView.js
@@ -1,10 +1,10 @@
+//function that gets a character object and returns the div element with all information about character
+
 const generatedDetailsCharacterView = function(character){
     let element = `<div>`;
     Object.keys(character).map(key => element = `${element}
     <h5>${key}: ${character[key]}</h5>`);
     element = `${element}
     </div>`;
-    console.log(element);
+    return element;
 }
-
-generatedDetailsCharacterView({ _id: "5a05e2b252f721a3cf2ea33f", name: "Gryffindor", mascot: "lion", headOfHouse: "Minerva McGonagall", houseGhost: "Nearly Headless Nick", founder: "Goderic Gryffindor"});
