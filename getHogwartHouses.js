554ab596d5c22ea733f9bf8bca4ee044bf48dad5
MikolajLangner <mikilan123@gmail.com>
2019-12-04 22:38:00 +0100

    Added plain, unstable yet function that returns all houses array

---
+++ getHogwartHouses.js
@@ -0,0 +1,13 @@
+//function that returns a list of house's objects with all properties
+
+function getHouse(){
+    let url = `https://www.potterapi.com/v1/houses/?key=$2a$10$SryMGjiskclp/nFOsb29Gu1yA9lM2mQwurU8YYNVf7CbzBi0QDwnm`;
+    return fetch(url)
+    .then(res => res.json())
+    .catch(er => {
+        console.log(er);
+        return er.message
+    })
+}
+
+console.log(getHouse());
