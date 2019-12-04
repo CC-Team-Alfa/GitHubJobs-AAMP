a3e936cc2895c6e450d13a8cf77b0474b6657464
MikolajLangner <57272921+MikolajLangner@users.noreply.github.com>
2019-12-04 18:31:20 +0100

    remove 'async'

--- scripts/getHouse.js
+++ scripts/getHouse.js
@@ -1,6 +1,6 @@
 //function that gets house's id and returns a house object with all properties
 
-async function getHouse(id){
+function getHouse(id){
     let url = `https://www.potterapi.com/v1/houses/${id}/?key=$2a$10$SryMGjiskclp/nFOsb29Gu1yA9lM2mQwurU8YYNVf7CbzBi0QDwnm`;
     return fetch(url)
     .then(res => res.json())
